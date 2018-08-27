a0026aa0fdf51370a4f29d37391cddd412b57534
Brent Smith <>
2018-08-27 11:36:11 -0600

    Learn JavaScript Requests - Requests I - HTTP Requests

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-var pasta = "Spaghetti"; // ES5 syntax
-
-const meat = "Pancetta"; // ES6 syntax
-
-let sauce = "Eggs and cheese"; // ES6 syntax
-
-// Template literals, like the one below, were introduced in ES6
-const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
+console.log('First message!');
+setTimeout(() => {
+   console.log('This message will always run last...');
+}, 0);
+console.log('Second message!');
