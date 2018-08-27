d6f835ae142960a56a37a8f26b75e34500442602
Brent Smith <>
2018-08-27 10:30:33 -0600

    Learn JavaScript Objects - Objects - Getters and Setters I

--- main.js
+++ main.js
@@ -1,43 +1,15 @@
 let person = {
-  name: 'Brent',
-  age: 30,
-  weekendAlarm: 'No alarms needed',
-  weekAlarm: 'Alarm set to 7AM',
+  _name: 'Lu Xun',
+  _age: 137,
 
-  sayHello: function() {
-    return `Hello, my name is ${this.name}`;
-  },
-
-  sayGoodbye() {
-    return 'Goodbye!';
+  set age(ageIn) {
+    if (typeof ageIn === 'number') {
+      this._age = ageIn;
+    }
+    else {
+      console.log('Invalid input');
+      return 'Invalid input';
+    }
   }
+
 };
-
-
-let friend = {
-  name: 'Barack'
-};
-
-friend.sayHello = person.sayHello;
-
-console.log(friend.sayHello());
-console.log(person.sayHello());
-
-person.hobbies = ['Playing Guitar', 'Cycling'];
-person.hobbies = ['Playing Guitar'];
-console.log(person.hobbies);
-
-
-console.log(person['name']);
-console.log(person['age']);
-
-let day = 'Tuesday';
-let alarm;
-
-if (day === 'Saturday' || day === 'Sunday' ) {
-  alarm = 'weekendAlarm';
-} else {
-  alarm = 'weekAlarm';
-}
-
-console.log(person[alarm]);
