22a561225923b37cf3a77b0c6628b47e44eb0020
Brent Smith <>
2018-08-27 03:55:17 -0600

    Learn JavaScript Arrays - Arrays - Property Access

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 let newYearsResolutions = ['Lose weight', 'Buy a house', 'Travel more'];
 
-console.log(newYearsResolutions);
+let listItem = newYearsResolutions[0];
+console.log(listItem);
+
+console.log(newYearsResolutions[2]);
+console.log(newYearsResolutions[3]);
