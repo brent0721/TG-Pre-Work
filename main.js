b4f4f05fc80eed122c8e62c88fdd446263066849
Brent Smith <>
2018-08-27 07:43:02 -0600

    Learn JavaScript Iterators - Iterators - Introduction to Iterators

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-let flag = true;
-let counter = 0;
-while(flag === true){
-  console.log(counter);
-  counter+=1;
-  if (counter === 37){
-    break;
-  }
-}
+let artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+
+artists.forEach(function(artist) {
+  console.log(artist + ' is one of my favorite artists.');
+});
+
+let numbers = [1, 2, 3, 4, 5];
+
+let squareNumbers = numbers.map(function(number) {
+  return number * number;
+});
+
+console.log(squareNumbers);
+
+let things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+
+let onlyNumbers = things.filter(function(thing) {
+  return typeof thing === 'number';
+});
+
+console.log(onlyNumbers);
