068e020bc5d8c2ae9586f43b6e38b2f634fa55a4
Brent Smith <>
2018-08-27 09:25:48 -0600

    Learn JavaScript Objects - Objects - Accessing Object Properties III

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 let person = {
   name: 'Brent',
-  age: 30
+  age: 30,
+  weekendAlarm: 'No alarms needed',
+  weekAlarm: 'Alarm set to 7AM'
 };
 
 console.log(person['name']);
 console.log(person['age']);
+
+let day = 'Tuesday';
+let alarm;
+
+if (day === 'Saturday' || day === 'Sunday' ) {
+  alarm = 'weekendAlarm';
+} else {
+  alarm = 'weekAlarm';
+}
+
+console.log(person[alarm]);
